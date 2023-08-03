d94b0a343b9c698ce43ada0a529db48a993810ce
KRISHNA PRASAD S <114407850+KrishnaPrasad05@users.noreply.github.com>
2023-08-03 20:47:34 +0530

    Update script.js

--- TogglePassword/JS/script.js
+++ TogglePassword/JS/script.js
@@ -4,12 +4,12 @@ const border=document.querySelector(".row");
 function showPass(){
     if(inputBox.type=="password"){
         inputBox.type="text";
-        eyeIcon.src="../IMG/show.png";
+        eyeIcon.src='../IMG/show.png';
         border.style.border="3.5px solid rgb(255, 0, 162)";
     }
     else{
         inputBox.type="password";
-        eyeIcon.src="../IMG/invisible.png";
+        eyeIcon.src='../IMG/invisible.png';
         border.style.border="none";
     }
 }
